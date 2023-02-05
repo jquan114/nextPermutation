e13140e6f7a7e2d68620852161640f5b29e78fa7
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-02-04 20:36:24 -0500

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,33 @@
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+
+function nextPermutation(nums) {
+  let i = nums.length - 2;
+  while (i >= 0 && nums[i] >= nums[i + 1]) {
+    i--;
+  }
+  if (i >= 0) {
+    let j = nums.length - 1;
+    while (j >= 0 && nums[j] <= nums[i]) {
+      j--;
+    }
+    swap(nums, i, j);
+  }
+  reverse(nums, i + 1, nums.length - 1);
+  return nums;
+}
+
+function swap(nums, i, j) {
+  let temp = nums[i];
+  nums[i] = nums[j];
+  nums[j] = temp;
+}
+
+function reverse(nums, start, end) {
+  while (start < end) {
+    swap(nums, start, end);
+    start++;
+    end--;
+  }
+}
